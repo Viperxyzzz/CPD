2b967c2b591f36b6b2d0ce2360f40a8b3b6744a0
Pedro MagalhAPSes Moreira Nunes <up201905396@fe.up.pt>
2022-03-15 09:58:21 +0000

    Matrix multiplication using python

---
+++ assign1/src/matrixproduct.py
@@ -0,0 +1,32 @@
+import time
+
+M_AR = 300
+M_BR = 300
+pha = [[0] * M_AR for i in range(M_AR)]
+phb = [[0] * M_AR for i in range(M_AR)]
+phc = [[0] * M_AR for i in range(M_AR)]
+
+def main():
+    for i in range(0, M_AR):
+        for j in range(0, M_AR):
+            pha[i][j] = 1.0
+
+
+    for i in range(0, M_BR):
+        for j in range(0, M_BR):
+            phb[i][j] = i+1.0
+
+
+    start_time = time.time()
+
+    for i in range(0, M_AR):
+        for j in range(0, M_BR):
+            temp = 0
+            for k in range(0, M_AR):
+                temp += pha[i][k] * phb[k][j]
+            phc[i][j] = temp
+    return start_time
+
+
+start_time = main()
+print("--- %s seconds ---" % (time.time() - start_time ))
