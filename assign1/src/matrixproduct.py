e5f7c9e163ed703c9c44a4e37469b2b8ec39cc3e
Luis Viegas <luisviegas@MBP-de-Luis-3.Home>
2022-03-27 21:09:06 +0100

    python code updated

--- assign1/src/matrixproduct.py
+++ assign1/src/matrixproduct.py
@@ -1,46 +1,54 @@
 import time
 
-M_AR = 300
-M_BR = 300
-pha = [[0] * M_AR for i in range(M_AR)]
-phb = [[0] * M_AR for i in range(M_AR)]
-phc = [[0] * M_AR for i in range(M_AR)]
 
-def main():
+def func(num):
+    M_AR = num
+    M_BR = num
+    return OnMultLine(M_AR, M_BR)
+
+
+def OnMult(M_AR, M_BR):
+    pha = [[0] * M_AR for i in range(M_AR)]
+    phb = [[0] * M_AR for i in range(M_AR)]
+    phc = [[0] * M_AR for i in range(M_AR)]
+    percentages = [i for i in range(M_AR // 10, M_AR, M_AR // 10)]
     for i in range(0, M_AR):
         for j in range(0, M_AR):
             pha[i][j] = 1.0
 
-
     for i in range(0, M_BR):
         for j in range(0, M_BR):
-            phb[i][j] = i+1.0
-
+            phb[i][j] = i + 1.0
 
     start_time = time.time()
 
     for i in range(0, M_AR):
+        if (i in percentages): print(i)
         for j in range(0, M_BR):
             temp = 0
             for k in range(0, M_AR):
                 temp += pha[i][k] * phb[k][j]
             phc[i][j] = temp
-    return start_time
+    return time.time() - start_time
 
-def OnMultLine():
+
+def OnMultLine(M_AR, M_BR):
+    pha = [[0] * M_AR for i in range(M_AR)]
+    phb = [[0] * M_AR for i in range(M_AR)]
+    phc = [[0] * M_AR for i in range(M_AR)]
+    percentages = [i for i in range(M_AR // 10, M_AR, M_AR // 10)]
     for i in range(0, M_AR):
         for j in range(0, M_AR):
             pha[i][j] = 1.0
 
-
     for i in range(0, M_BR):
         for j in range(0, M_BR):
-            phb[i][j] = i+1.0
-
+            phb[i][j] = i + 1.0
 
     start_time = time.time()
 
     for j in range(0, M_BR):
+        if (j in percentages): print(j)
         for i in range(0, M_AR):
             temp = 0
             for k in range(0, M_AR):
@@ -48,5 +56,11 @@ def OnMultLine():
             phc[i][j] = temp
     return time.time() - start_time
 
-run_time = OnMultLine()
-print("--- %s seconds ---" % ( run_time))
+
+def main():
+    for i in range(1000, 3001, 400):
+        run_time = func(i)
+        print("--- %s seconds ---" % (run_time))
+
+
+main()
